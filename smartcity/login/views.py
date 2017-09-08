54574879c8d12fa10e9eecae9e55cd83b6883ab5
Matthew Swan <mattswan@live.com>
2017-09-08 11:55:26 +1000

    Created forms for logging in and registering
    Forms can be accessed via '/login/' and '/login/register'.

---
+++ smartcity/login/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'login/index.html')
+
+def register(request):
+    return render(request, 'login/register.html')
